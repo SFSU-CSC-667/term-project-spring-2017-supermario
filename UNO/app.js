e67fed12934e99fd50a023a6e22e9c1e0fe1b70b
MagicalDonald <magicaldonald@gmail.com>
2017-04-22 10:39:44 -0700

    Login use databese

--- UNO/app.js
+++ UNO/app.js
@@ -5,12 +5,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
 const passport = require('passport')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(flash());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- UNO/authentication/passport.js
+++ UNO/authentication/passport.js
@@ -1,6 +1,10 @@
 // This initializes and implements passport methods
 var passport = require( 'passport' );
 var LocalStrategy = require( 'passport-local' ).Strategy;
+const pgp = require( 'pg-promise' )({
+});
+connection = "postgres://localhost:5432/UNO"
+db=pgp(connection);
 
 // Test users
 var users = {
@@ -22,20 +26,28 @@ passport.deserializeUser(function(user, done) {
 var localStrategy = new LocalStrategy({
       usernameField: 'email',
       passwordField: 'password',
-    },
-    function(username, password, done) {
-      user = users[ username ];
-
-      if ( user == null ) {
+	  passReqToCallback : true
+   },
+    function(req, username, password, done) {
+      db.one('SELECT * FROM users where email like $1 ', username).then(
+       user => {
+	  console.log(username)
+      console.log(user)
+      /*if ( user == null ) {
         return done( null, false, { message: 'Invalid user' } );
       };
 
-      if ( user.password !== password ) {
+      if ( user.encrypted_password !== password ) {
         return done( null, false, { message: 'Invalid password' } );
-      };
+      };*/
 
       done( null, user );
-    }
+      })
+       .catch(error => {
+        // error; 
+ 		 console.log(error);
+      });
+   }
   )
 
 passport.use( 'local', localStrategy );
