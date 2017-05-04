b74054e2bbcc4cf482c82c5d5dfff0100cd54c97
carlott <carlott99@gmail.com>
2017-05-04 12:56:35 -0700

    game developing

--- UNO/controllers/game/index.js
+++ UNO/controllers/game/index.js
@@ -1,4 +1,5 @@
 const socketIo = require('socket.io');
+const db = require('../../database/game');
 
 const game = (app, server) => {
   const io = socketIo(server);
@@ -16,10 +17,53 @@ const game = (app, server) => {
         socket.emit('chat message', msg);
       });
 
+var result;
       socket.on('game', function(msg) {
-        socket.emit('game', 'Echo from game server:' + msg);
+        result = getData(msg.word.toString());
+        // next line is for testing use
+        console.log(JSON.stringify(msg));
+        console.log(JSON.stringify(result));
+        socket.emit('game', 'Echo from game server:' + JSON.stringify(result));
       });
   });
+}
+
+function getData(word) {
+  console.log(word)
+  switch (word) {
+    case 'cardImages':
+      return db.cardImages;
+      break;
+    case 'cardId':
+      return db.cardId;
+      break;
+    case 'myInfo':
+      return 'get myInfo';
+      break;
+    case 'playersInfo':
+      return 'get playersInfo';
+      break;
+    case 'playersState':
+      return 'get playersState';
+      break;
+    case 'skip':
+      return 'get skip';
+      break;
+    case 'ready':
+      return 'get ready';
+      break;
+    case 'red':
+      return 'get red';
+      break;
+    case 'uno':
+      return 'get uno';
+      break;
+    case 'exit':
+      return 'get exit';
+      break;
+    default:
+      return 'no matched word';
+  }
 }
 
 module.exports = { game };
